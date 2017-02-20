3ed1c26b95ad477bb1392ec4c574c4eef75523b1
Andrew Shaw Care <andrewshawcare@gmail.com>
2017-02-19 21:36:59 -0500

    Contact element has an id

--- src/contact-element/index.js
+++ src/contact-element/index.js
@@ -1,6 +1,12 @@
-module.exports = () => {
+module.exports = ({ id = '' } = {}) => {
   const contactElement = document.createElement('section');
   contactElement.classList.add('contact');
+
+  const idElement = document.createElement('input');
+  idElement.classList.add('id');
+  idElement.setAttribute('type', 'hidden');
+  idElement.value = id;
+  contactElement.appendChild(idElement);
 
   return contactElement;
 };
--- src/contact-element/test.js
+++ src/contact-element/test.js
@@ -5,4 +5,9 @@ describe('Contact element', () => {
   it('has the contact class', () => {
     expect(ContactElement().classList.contains('contact')).toBe(true);
   });
+
+  it('has an id', () => {
+    const id = '1';
+    expect(ContactElement({ id }).querySelector('.id').value).toBe(id);
+  });
 });
