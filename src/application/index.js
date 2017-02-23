38a802c4339ccc64ad2414a2b05af249e39d02fb
Andrew Shaw Care <andrewshawcare@gmail.com>
2017-02-23 14:57:30 -0500

    Refactor query declaration

--- src/application/index.js
+++ src/application/index.js
@@ -1,10 +1,9 @@
 const List = require('../list');
 const defaultContacts = require('./default-contacts.json');
 
-module.exports = ({ contacts = defaultContacts } = {}) => {
+module.exports = ({ contacts = defaultContacts, query = '' } = {}) => {
   const isActiveContact = (contact) => contact.active;
   const contactList = List({ items: contacts });
-  let query = '';
   return {
     contacts () {
       return contactList.toArray();
